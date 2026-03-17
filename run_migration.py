faf3a5f13921e0bb5355b47cba2c813e880ab398
genorman <genevieve.norman@sentry.io>
2026-03-17 13:03:51 -0700

    add check_explore, fix validate

--- run_migration.py
+++ run_migration.py
@@ -1,5 +1,5 @@
 cd ~/looker-swap
-pbpaste > run_migration.py
+pbpaste > run_migration.py   # copy from artifact first
 git add run_migration.py
-git commit -m "add check_explore and validate fixes"
+git commit -m "add check_explore, fix validate"
 git push
